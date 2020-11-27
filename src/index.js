4d995ac3979ce4080b2af118ffcd6e74d6768c4d
Hannes Fostie <hannes.fostie@gmail.com>
2020-11-27 13:38:30 +0100

    add tailwind css setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EACalc from './EACalc';
 import reportWebVitals from './reportWebVitals';
+import './tailwind.output.css';
 
 ReactDOM.render(
   <React.StrictMode>
